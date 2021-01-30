f9aa06247ef720b6936fec9553360b6d267c75a8
usernameAshu <mohantyashu.07@gmail.com>
2021-01-30 14:24:26 +0530

    initial setup code

---
+++ src/main/java/CsvDriver.java
@@ -0,0 +1,5 @@
+public class CsvDriver {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/CsvDriverTest.java
@@ -0,0 +1,40 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class CsvDriverTest {
+
+    public final static Map<String, String> AUTHOR_BOOK_MAP =
+            Collections.unmodifiableMap(new LinkedHashMap<String, String>() {
+                {
+                    put("Dan Simmons", "Hyperion");
+                    put("Douglas Adams", "The Hitchhiker's Guide to the Galaxy");
+                }
+            });
+    public final static String[] HEADERS = {"author", "title"};
+    public final static String EXPECTED_FILESTREAM =
+            "author,title\r\n" + "Dan Simmons,Hyperion\r\n" + "Douglas Adams,The Hitchhiker's Guide to the Galaxy";
+
+    @Test
+    void testReadCsv() throws IOException {
+        Reader in = new FileReader(
+                "E:\\personal-projects\\java-codes\\apache-commons-csv-poc\\src\\test\\resources\\books.csv");
+        Iterable<CSVRecord> csvRecords = CSVFormat.DEFAULT
+                .withHeader(HEADERS)
+                .withFirstRecordAsHeader()
+                .parse(in);
+        for (CSVRecord record : csvRecords) {
+            String author = record.get("author");
+            String title = record.get("title");
+            Assertions.assertEquals(AUTHOR_BOOK_MAP.get(author), title);
+        }
+    }
+}
